98df6cacc533caf58ddb0d2659de92b21e894199
Miranda Mafe <miranda.mafe@gmail.com>
2019-09-17 11:44:08 -0400

    Take behat screenshot on fail

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -5,6 +5,9 @@ use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Mink\Driver\Selenium2Driver;
+use Behat\MinkExtension\Context\RawMinkContext
 
 /**
  * Defines application features from the specific context.
@@ -19,5 +22,41 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    * context constructor through behat.yml.
    */
   public function __construct() {
+  }
+
+ /**
+   * @AfterStep
+   */
+  public function printLastResponseOnError(AfterStepScope $event)
+  {
+      if (!$event->getTestResult()->isPassed()) {
+          $this->saveDebugScreenshot();
+      }
+  }
+
+  /**
+   * @Then /^save screenshot$/
+   */
+  public function saveDebugScreenshot()
+  {
+      $driver = $this->getSession()->getDriver();
+
+      if (!$driver instanceof Selenium2Driver) {
+          return;
+      }
+
+      if (!getenv('BEHAT_SCREENSHOTS')) {
+          return;
+      }
+
+      $filename = microtime(true).'.png';
+      $path = $this->getContainer()
+          ->getParameter('kernel.root_dir').'/../behat_screenshots';
+
+      if (!file_exists($path)) {
+          mkdir($path);
+      }
+
+      $this->saveScreenshot($filename, $path);
   }
 }
